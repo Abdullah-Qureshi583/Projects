69dfdd0d5396102f80e14315aa46c1c463b7bcb0
Abdullah-Qureshi583 <mabdullahqureshi583@gmail.com>
2024-10-20 03:02:30 +0500

    Next js Projects

--- NextJsBasedProjects/password-manager/src/app/layout.js
+++ NextJsBasedProjects/password-manager/src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Script from "next/script";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,11 +19,13 @@ export const metadata = {
   description:
     "This is the password manager that save your password to your own local storage",
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.png" sizes="30" />
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="30" />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
